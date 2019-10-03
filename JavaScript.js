bf310215682a05544a94211adf306e97fd267569
emilemoosman <emilemoosman@byui.edu>
2019-10-03 10:19:03 +1000

    JavaScript evidence draft 1

--- JavaScript.js
+++ JavaScript.js
@@ -1,32 +1,71 @@
+/**
+	Functions
+**/
 
-function saveNote(){
-  		var currentDateAndTime = new Date()
-  		var aNoteDescription = document.getElementById("description_input").value
-  		var aNoteText = document.getElementById("note_editor").value
-  		var aCompleteNote = currentDateAndTime.toLocaleString() + "--" + aNoteDescription 
-  		aCompleteNote += "<p>" + aNoteText + "</p>"
-		var storedNotesString = localStorage.getItem("all_notes")
-		var allNotes = JSON.parse(storedNotesString)
-		if(allNotes == null){
-			allNotes = []
-		}
-		allNotes.push(aCompleteNote)
-		var allNotesString = JSON.stringify(allNotes)
-		localStorage.setItem("all_notes",allNotesString)
-		showAllNotes()
-		document.getElementById("description_input").value = null
-		document.getElementById("note_editor").value = null
-  	}
-  	function showAllNotes(){
-		var storedNotesString = localStorage.getItem("all_notes")
-		allNotes = JSON.parse(storedNotesString)
-		if (allNotes != null){
-	  		var noteDisplayer = document.getElementById("all_notes_display")
-	  		noteDisplayer.innerHTML = null
-	  		var numberOfNotes = allNotes.length
-	  		for (var i = 0; i < allNotes.length; i++) {
-	  			var aNote = allNotes[i]
-	  			noteDisplayer.innerHTML += "<hr><p>"+ aNote +"</p>"
-	  		}
-	  	}
-  	}
+/**
+	name: performAddition
+	Retrieves user input, calculates and displays the sum.
+	parameters: none.
+	return: nothing.
+**/
+function performAddition(){
+	
+	// Constant variables
+	const FIRST  = 1;
+	const SECOND = 2;
+	
+	// Variables
+	let sum;
+	
+	// Arrays
+	var arrayOne = [1, 6, 13, 45, 22, 18];
+	var arrayTwo = [90, 5, 41, 26, 15, 8];
+	
+	// Conditionals
+	if (document.getElementById("input").value == FIRST)
+		sum = returnSum(arrayOne);		// function call
+	else
+		sum = returnSum(arrayTwo);		// function call
+	
+	//
+	displaySum(sum);
+	
+}
+
+/**
+	name: returnSum
+	Returns the sum of each element in array parameter.
+	parameters: An array of numbers.
+	return: a sum.
+**/
+function returnSum(array) {
+	
+	//
+	var sum = 0;
+	
+	// Loops
+	for (i = 0; i < array.length; i++) {
+		console.log("array[" + i + "] = " + array[i]);
+		//
+		sum += array[i];
+	}
+	
+	//
+	return sum;
+}	
+
+
+/**
+	name: displaySum
+	Display the calculated sum.
+	parameters: An number representing the calculated sum.
+	return: nothing.
+**/
+function displaySum(sum) {
+	
+	//
+	var sumDisplayer = document.getElementById("display_sum");
+	//sumDisplayer.innerHTML = null;
+	sumDisplayer.innerHTML = "<hr><p>The sum is "+ sum +"</p>";
+
+}	 		
