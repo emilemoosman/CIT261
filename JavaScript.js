d9b2c821073ef393fd299ef10f0d19c572ef3696
emilemoosman <emilemoosman@byui.edu>
2019-10-03 10:38:10 +1000

    JavaScript evidence complete

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,7 @@
 /**
-	Functions
+	Functions: a block of code designed to perform a particular task. 
+	A JavaScript function is executed when "something" invokes it (calls it).
+	The parentheses may include parameter names separated by commas. 
 **/
 
 /**
@@ -10,26 +12,40 @@
 **/
 function performAddition(){
 	
-	// Constant variables
+	// Constant variable - a value that once declared can never be changed.
 	const FIRST  = 1;
-	const SECOND = 2;
 	
-	// Variables
+	/**
+		Variables: Containers for storing data values. You start by 
+		declaring a variable with the var or the let keyword, followed 
+		by any name you want to call it. Variables can contain just about 
+		anything — not just strings and numbers. Variables can also contain 
+		complex data and even entire functions to do amazing things.
+	**/
 	let sum;
 	
-	// Arrays
+	/**
+		Arrays arrays are used to store multiple values in a single variable. 
+		An array is a special variable, which can hold more than one value at 
+		a time. 
+		Arrays are a special type of objects. You can have objects in an Array. 
+		You can have functions in an Array. You can have arrays in an Array.
+	**/
 	var arrayOne = [1, 6, 13, 45, 22, 18];
 	var arrayTwo = [90, 5, 41, 26, 15, 8];
 	
-	// Conditionals
+	/**
+		Conditionals - Code structures which allow you to test if an expression 
+		is true, and then run alternative code depending on the result. 
+	**/
 	if (document.getElementById("input").value == FIRST)
 		sum = returnSum(arrayOne);		// function call
 	else
 		sum = returnSum(arrayTwo);		// function call
 	
-	//
+	// function call, which accepts the sum as a parameter,
+	// and will display the sum in the html.
 	displaySum(sum);
-	
 }
 
 /**
@@ -40,10 +56,11 @@ function performAddition(){
 **/
 function returnSum(array) {
 	
-	//
+	// declare and initialize a variable.
 	var sum = 0;
 	
-	// Loops
+	// Loops: Execute a block of code a number of times. They include
+	// the do-while, while, and for loop structures.
 	for (i = 0; i < array.length; i++) {
 		console.log("array[" + i + "] = " + array[i]);
 		//
@@ -63,9 +80,9 @@ function returnSum(array) {
 **/
 function displaySum(sum) {
 	
-	//
+	// store the document element id in a variable.
 	var sumDisplayer = document.getElementById("display_sum");
-	//sumDisplayer.innerHTML = null;
+	// display in the relevant element, the below string.
 	sumDisplayer.innerHTML = "<hr><p>The sum is "+ sum +"</p>";
 
 }	 		
