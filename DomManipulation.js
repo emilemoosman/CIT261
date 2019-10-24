cfd5a2939da475c63c39090af4693a08ac893a7c
emilemoosman <emilemoosman@byui.edu>
2019-10-24 23:27:39 +1100

    Finished DOM and CSS

--- DomManipulation.js
+++ DomManipulation.js
@@ -3,10 +3,9 @@
 // Paragraphs counter
 var counter = 0;
 
-
-
 /**************************************************
-	Create paragraph HTML elements
+	Create paragraph HTML elements using the 
+	createElement() method.
 **************************************************/
 function createParagraphs() {
 	
@@ -27,12 +26,6 @@ function createParagraphs() {
 	
 	// Added two paragraphs, so increment counter by 2.
 	counter += 2;
-	
-	//
-	var buttons = document.getElementsByTagName("BUTTON");
-	for (var i = 0; i < buttons.length; i++)
-		buttons[i].style.backgroundColor = "yellow";
-	
 }
 
 /**************************************************
@@ -55,11 +48,6 @@ function insertParagraph() {
 	
 	// Added one paragraph, so increment counter by 1.
 	counter++;
-	
-	//
-	var buttons = document.getElementsByTagName("BUTTON");
-	for (var i = 0; i < buttons.length; i++) 
-		buttons[i].style.color = "blue";
 }
 
 /**************************************************
@@ -77,18 +65,14 @@ function removeParagraph() {
 		paragraphs.removeChild(paragraphs.childNodes[0]);
 		// decrement paragraph counter by 1.
 		counter--;
-		//
-		document.getElementById("content").style.fontFamily = "Helvetica";
 	}	else {
 		console.log("Cannot remove, as there are no paragraphs.");	
-		//
-		document.getElementById("content").style.fontFamily = "fantasy";
 	}
 }
 
 /**************************************************
-	Replace an HTML element using the replaceChild()
-	method.
+	Replace an HTML element with another element using 
+	the replaceChild() method.
 **************************************************/
 function replaceParagraph() {
 	
@@ -101,12 +85,31 @@ function replaceParagraph() {
 		// Insert text into new HTML element.
 		para.innerHTML = "This is a REPLACE paragraph.";      		
 		// Remove paragraphs first child node (index 0)
-		paragraphs.replaceChild(para, paragraphs.childNodes[0]); 
-		//
-		document.getElementById("myList").style.visibility = "hidden";		
+		paragraphs.replaceChild(para, paragraphs.childNodes[0]); 	
 	} else {
 		console.log("Cannot replace, as there are no paragraphs.");	
-		//
-		document.getElementById("myList").style.visibility = "visible";
 	}
+}
+
+/**************************************************
+	This method will cover the code topic,
+	'Manipulating CSS Class Properties Using JavaScript'.
+	The classList property returns the class name(s) of an 
+	element, as a DOMTokenList object. This property is useful 
+	to add, remove and toggle CSS classes on an element.
+	The classList property is read-only, however, you can 
+	modify it by using the add() and remove() methods.
+	The classList property is not supported in IE9 and earlier. 
+	This method will focus on the toggle feature of using classList.
+**************************************************/
+function cssManipulation() {
+	// get the element IDs
+	var bodyElement = document.getElementById("myContent");
+	var listElement = document.getElementById("myList");
+	var linkElement = document.getElementById("myLink");
+   // when function is called, toggle back and forth
+	// from the CSS .class passed as a parameter.
+	bodyElement.classList.toggle("myBodyStyle");
+   listElement.classList.toggle("myListStyle");
+	linkElement.classList.toggle("myLinkStyle");	
 }
