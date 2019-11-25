5bc38f1cc8eb1d136b49a381616b802b8cdb1666
emilemoosman <emilemoosman@byui.edu>
2019-11-25 18:54:24 +1100

    added enter key event listener

--- WeatherProject.js
+++ WeatherProject.js
@@ -1,6 +1,17 @@
 // Event Listener for button
 document.getElementById("clickMe").addEventListener("click", cityWeather);
 
+// Execute a function when the user releases a key on the keyboard (enter key)
+document.getElementById("cityID").addEventListener("keyup", function(event) {
+	// Number 13 is the "Enter" key on the keyboard
+	if (event.keyCode === 13) {
+		// Cancel the default action, if needed
+		event.preventDefault();
+		// Trigger the button element with a click
+		document.getElementById("clickMe").click();
+	}
+});
+
 /*************************************************
  * This function executes when the button is clicked.
  ************************************************/
